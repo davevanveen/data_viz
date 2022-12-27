b5dc33499e56ae6c877d904e53be7102c72cc734
Dave Van Veen <davemvanveen@gmail.com>
2022-12-27 00:42:43 -0800

    generate wordcloud

---
+++ tomato_wordcloud.py
@@ -0,0 +1,71 @@
+''' given a .csv file downloaded from https://mytomatoes.com,
+    this script extracts and conjugates the verbs,
+    then outputs them to a .txt file 
+
+    subsequently, i recommend generating a point cloud by
+    uploading the .txt file to https://wordclouds.com '''
+
+
+import os, sys
+import pandas as pd
+import nltk # https://pypi.org/project/nltk/
+from nltk.stem.wordnet import WordNetLemmatizer as WNL
+nltk.download('wordnet')
+
+
+def main():
+
+    # read input csv, extract activity summary 
+    df = pd.read_csv('csv/mytomatoes.csv',
+                     sep=',',
+                     names=['start', 'end', 'did'])
+    did = df['did'].tolist()
+
+    # extract and conjugate verbs
+    verbs_conjug = get_conjug_verb_list(did)
+
+    # write to txt
+    with open('verbs.txt', mode='wt', encoding='utf-8') as f:
+        f.write('\n'.join(verbs_conjug))
+
+
+def get_conjug_verb_list(did):
+    ''' given a list of did (activities),
+        extract verbs and conjugate to present tense
+        note: assumes verb is first word in did column '''
+
+    # from each entry, extract first word
+    # note: assumes first word is verb
+    verbs = [s.split(' ')[0] for s in did]
+
+    # conjugate verbs to present tense
+    verbs_conjug = [WNL().lemmatize(s,'v') for s in verbs]
+
+    # define manual substitutions to correct typos + ntlk errors
+    subs = {
+            'analyzew': 'analyze',
+            'attend': '',
+            'buidl': 'build',
+            'crete': 'create',
+            'derivwe': 'derive',
+            'disucss': 'discuss',
+            'eval': 'evaluate',
+            'instal': 'install',
+            'meet': '',
+            'organizer': 'organize',
+            'organizew': 'organize',
+            'prep': 'prepare',
+            'prepped': 'prepare',
+            'prepping': 'prepare',
+            'robustified': 'robustify',
+            'thikn': 'think',
+    }
+
+    # make substitutions
+    verbs_conjug = [subs.get(i,i) for i in verbs_conjug]
+    assert len(did) == len(verbs_conjug)
+
+    return verbs_conjug
+
+if __name__ == '__main__':
+    main()
